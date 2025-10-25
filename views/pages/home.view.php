aa923bc0364df831f8eabeccba104c5d46dc1ccf
Vyygir <open.source@vyygir.me>
2025-10-25 23:47:56 +0100

    improve: fix default meta description

--- views/pages/home.view.php
+++ views/pages/home.view.php
@@ -2,7 +2,7 @@
 	<meta property="og:title" content="Starless" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://starle.sh" />
-	<meta property="og:description" content="Starless" />
+	<meta property="og:description" content="Entries from the dark side of engineering" />
 	<meta property="og:image" content="https://starle.sh/static/meta/image.png" />
 </x-slot>
 
